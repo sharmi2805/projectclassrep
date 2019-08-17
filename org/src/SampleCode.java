db5de9c07e1ce02bdd528fc0dd015e1c8a30cdb6
sharmi2805 <sharmi28sp@gmail.com>
2019-08-17 14:21:37 +0530

    code

---
+++ org/src/SampleCode.java
@@ -0,0 +1,18 @@
+package org.src;
+
+public class SampleCode {
+ public void id() {
+System.out.println("new123");
+ }
+public void name() {
+System.out.println("deepa");
+ }
+public static void main(String[] args) {
+	SampleCode s = new SampleCode();
+	s.id();
+	s.name();
+	
+	
+}
+
+}
